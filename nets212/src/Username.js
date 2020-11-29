af33dd78220e0e29fcc235eda16d2cc97385d228
Henrique <henrique.lorente@gmail.com>
2020-11-29 10:43:47 -0500

    fixed username

--- nets212/src/Username.js
+++ nets212/src/Username.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 function Username(props) {
     return (
         <div>
-            <a href={props.userURL}>{props.name}</a>
+            <a href={props.userURL}>{props.firstName} {props.lastName}</a>
         </div>
     )
 }
