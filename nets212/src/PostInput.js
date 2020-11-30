6c10f1c4cb948979a0fda9ae4c22d3ab26b389e4
Pranav Aurora <pranava@seas.upenn.edu>
2020-11-30 00:23:04 -0500

    Updated UserInput

--- nets212/src/PostInput.js
+++ nets212/src/PostInput.js
@@ -1,24 +1,38 @@
-import React, { userRef } from 'react'
+import React, { useState } from 'react'
 
 
 function PostInput() {
+    const [state , setState] = useState({
+        text : "",
+        imageUrl : "",
+    })
+
+    const handleChange = e => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
 
     const handleClick = e => {
-        e.preventDefault();  
+        e.preventDefault();
+
+        //need to upload the image to the s3 bucket, get the URl, and set the state.
+        
+        
     };
     return (
-        <form className = 'upload-steps' onSubmit = {handleClick}>
-            <input className ="form-control input-lg" type = 'text' placeholder="what's on your mind?"/> 
-            <div className="input-group mb-3">
-                <div className="custom-file">
-                    <input type="file" className="custom-file-input" id="inputGroupFile02"/>
-                    <label className="custom-file-label" for="inputGroupFile02">choose image</label>
+            <div>
+                <input className ="form-control input-lg" type = 'text' placeholder= "what's on your mind?" id = "text" onChange={handleChange}/> 
+                <div className="input-group mb-3">
+                    <div className="custom-file">
+                        <input type="file" className="custom-file-input" id="inputGroupFile02"/>
+                        <label className="custom-file-label" for="inputGroupFile02">choose file</label>
+                    </div>
                 </div>
-                <div className="input-group-append">
-                    <span className="input-group-text" id="">Upload</span>
-                </div>
-          </div>
-        </form>
+                <button className="btn btn-primary" onClick={handleClick}>Post</button>
+            </div>
     );
 }
 
