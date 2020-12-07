1ecee542874861ea415d1bbf40e3224c4b9348a8
Pranav Aurora <pranava@seas.upenn.edu>
2020-12-07 00:51:56 -0500

    message update

--- nets212/src/Message.js
+++ nets212/src/Message.js
@@ -11,19 +11,26 @@ const people = [
         this.state = {}
     }
     render(){
-        return (
-            <div>
-                <div className="card text-right" style={{backgroundColor:"#6fa0ed", borderRadius:"25px", paddingLeft:"15px" }}>
+        let card;
+        if (this.props.sent === "true") {
+            card = <div className="card text-right" style={{backgroundColor:"#6fa0ed", borderRadius:"25px", paddingLeft:"15px", paddingBottom:"0px" }}>
                     <div class="card-body">
                         sent message.
                     </div>
-                </div>
-                <div className="card text-left" style={{borderRadius:"25px", paddingRight:"15px"}}>
+                    </div>
+        } else {
+            card = <div className="card text-left" style={{borderRadius:"25px", paddingRight:"15px", backgroundColor:"#b5b7ba"}}>
+                    
                     <div class="card-body">
                         received message
                     </div>
                 </div>
+        }
+        return (
+            <div>
+                {card}
             </div>
+
         )
     }
 
