803ce4c4c42488b199b237a07a2d2c7e6360e604
Henrique <henrique.lorente@gmail.com>
2020-12-14 05:18:19 -0300

    uploading pictures

--- nets212/src/PostInput.js
+++ nets212/src/PostInput.js
@@ -12,56 +12,69 @@ const config = {
     secretAccessKey: 'G4lWU/Oja0p/SxMJa7+Uife9ssL8uOBstOMn7QbQ'
 }
 
-function PostInput(props) {
-    const [state , setState] = useState({
-        text : "",
-        imageUrl : "",
-    })
+class PostInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text : "",
+            pictures : [],
+            uploading: false
+        }
+    }
 
-    const handleChange = e => {
+    handleChange = e => {
         const {id , value} = e.target   
-        setState({
+        this.setState({
             text: value
         });
     }
 
-    const handleClick = e => {
+    handleClick = e => {
         e.preventDefault(); 
-        let post = {
-            text: state.text,
-            pictures: [state.imageUrl],
-            author: localStorage.getItem('user'),
-            privacy: 0,
-            parent: "0"
+        if(!this.state.uploading) {
+            let post = {
+                text: this.state.text,
+                pictures: this.state.pictures,
+                author: localStorage.getItem('user'),
+                privacy: 0,
+                parent: "0"
+    
+            };
+            let request = $.post(Config.serverUrl + '/posts/create', post);
+            request.done((result) => {
+                //Make post show on newsfeed
+                this.props.addPost(result.id);
+            });
+        } else {
+            alert("Image still uploading!");
+        }
 
-        };
-        let request = $.post(Config.serverUrl + '/posts/create', post);
-        request.done((result) => {
-            //Make post show on newsfeed
-            props.addPost(result.id);
-        });
     };
 
-    const fileChange = e => {
-        S3FileUpload.uploadFile(e.target.files[0], config).then((data)=> { setState({imageUrl: data.location}) }).catch((err)=> {alert(err)})
+    fileChange = e => {
+        this.setState({uploading:true});
+        S3FileUpload.uploadFile(e.target.files[0], config).then((data)=> { this.setState({pictures: [data.location], uploading:false })}).catch((err)=> {alert(err)})
     }
 
-    return (
-        <div class="container-fluid p-0 mt-3">
-            <div className="input-group mb-3 p-0">
-                <input id="theInput" className ="form-control input-lg m-0 pb-0 pt-0" type = 'text' placeholder= {props.user ? "Post on " + props.user.firstName + "'s wall" : "What's on your mind?"} onChange={handleChange}/> 
-                <div className="input-group-append m-0">
-                    <button className="btn btn-primary m-0" style={{width:79}} onClick = {handleClick}> Post </button>
+    render() {
+        return (
+            <div class="container-fluid p-0 mt-3">
+                <div className="input-group mb-3 p-0">
+                    <input id="theInput" className ="form-control input-lg m-0 pb-0 pt-0" type = 'text' placeholder= {this.props.user ? "Post on " + this.props.user.firstName + "'s wall" : "What's on your mind?"} onChange={this.handleChange}/> 
+                    <div className="input-group-append m-0">
+                        <button className="btn btn-primary m-0" style={{width:79}} onClick = {this.handleClick}> Post </button>
+                    </div>
                 </div>
+                    <div className="custom-file m-0">
+                        <h3>Choose an image:</h3>
+                        <input type="file" accept="image/x-png,image/gif,image/jpeg" onChange= {this.fileChange} />
+                       
+                    </div>
             </div>
-                <div className="custom-file m-0">
-                    <h3>Choose an image:</h3>
-                    <input type="file" accept="image/x-png,image/gif,image/jpeg" onChange= {fileChange} />
-                   
-                </div>
-        </div>
-            
-    );
+                
+        );
+    }
+
 }
 
 export default PostInput;
