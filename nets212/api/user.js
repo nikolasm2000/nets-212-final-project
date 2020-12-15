9716686afb0aeedd8f7ebf68e320097902ce3d9e
rfax <rmarques@seas.upenn.edu>
2020-12-14 22:56:48 -0300

    add debug to update

--- nets212/api/user.js
+++ nets212/api/user.js
@@ -62,6 +62,7 @@ var create = function(req,res){
 
 var update = function(req,res){
     req.body.id = req.session.user;
+    console.log("data received:", data.body);
     db.user.update(req.body,db.dataCallback(res));
 }
 
