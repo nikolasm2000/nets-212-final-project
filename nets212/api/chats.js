7dd944af964a80b40b22a70bae7c1208e73877bf
rfax <rmarques@seas.upenn.edu>
2020-12-17 16:52:37 -0300

    chat v i dont even know anymore

--- nets212/api/chats.js
+++ nets212/api/chats.js
@@ -3,8 +3,33 @@ const { chatMembers, callbackSkeleton } = require('./database.js');
 
 db = require('./database.js');
 
-var getPrivateChat = function(curr_user, user){
-
+var getPrivateChat = function(req, res){
+    db.chats
+    .query(req.session.user)
+    .usingIndex("UserIndex")
+    .where('user2').equals(req.params.id)
+    .limit(1)
+    .exec(callbackSkeleton(res, function(data){
+        if(data.Count > 0){
+            invite(data.Items[0].attrs.id, req, res);
+        } else {
+            db.chats
+            .query(req.session.user)
+            .usingIndex("UserInvIndex")
+            .where('user1').equals(req.params.id)
+            .limit(1)
+            .exec(callbackSkeleton(res, function(data){
+                if(data.Count > 0){
+                    invite(data.Items[0].attrs.id, req, res);
+                } else {
+                    //create new chat if none exists
+                    db.chats.create({private: true, user1: req.session.user, user2: req.params.id}, db.callbackSkeleton(res, function(chatdata){
+                        invite(chatdata.attrs.id, req, res);
+                    }));
+                }
+            }));
+        }
+    }));
 }
 
 var createNewChat = function(req, res){
@@ -44,37 +69,37 @@ var getChatUsersRoute = function(req, res){
     }));
 }
 
-var invite = function(req, res){
-    db.chats.create({}, db.callbackSkeleton(res, function(chatdata){
-        params1 = {"chat": chatdata.attrs.id, "PBuser": req.session.user}
-        params2 = {"chat": chatdata.attrs.id, "PBuser": req.params.id}
-        db.chatMembers.create([params1, params2], db.callbackSkeleton(res, function(data){
-            var params3 = {
-                "PBuser": req.session.user,
-                "type": 0,
-                "text": "Your chat session has been created",
-                "urlText": "Click here to join the session",
-                "url": "/chats/" + chatdata.attrs.id,
-                "dismissed" : false,
-                "read" : false
-            }
-            var params4 = {
-                "PBuser": req.params.id,
-                "type": 2,
-                "relevant_id": req.session.user,
-                "url": "/chats/" + chatdata.attrs.id,
-                "dismissed" : false,
-                "read" : false
-            }
-            db.notifications.create([params3,params4],db.callbackSkeleton(res,function(data){
-                res.json(chatdata);
-            }));
-        }))
-    }))
+var invite = function(id, req, res){
+    
+    //if no chat exists
+    params1 = {"chat": id, "PBuser": req.session.user}
+    params2 = {"chat": id, "PBuser": req.params.id}
+    db.chatMembers.create([params1, params2], db.callbackSkeleton(res, function(data){
+        var params3 = {
+            "PBuser": req.session.user,
+            "type": 0,
+            "text": "Your chat session has been created",
+            "urlText": "Click here to join the session",
+            "url": "/chats/" + id,
+            "dismissed" : false,
+            "read" : false
+        }
+        var params4 = {
+            "PBuser": id,
+            "type": 2,
+            "relevant_id": req.session.user,
+            "url": "/chats/" + id,
+            "dismissed" : false,
+            "read" : false
+        }
+        db.notifications.create([params3,params4],db.callbackSkeleton(res,function(data){
+            res.json({'id': id});
+        }));
+    }));
 }
 
 var saveMessage = function(user, chat, message, callback){
-    db.messages.create({"chat": chat, "PBuser": user, "text": message}, callback);
+    db.messages.create({"chat": chat, "PBuser": user, "message": message}, callback);
 }
 
 var chats = {
@@ -83,7 +108,8 @@ var chats = {
     getChatUsersRoute: getChatUsersRoute,
     saveMessage: saveMessage,
     invite: invite,
-    getAllMessages: getAllMessages
+    getAllMessages: getAllMessages,
+    getPrivateChat: getPrivateChat
 };
 
 module.exports = chats;
